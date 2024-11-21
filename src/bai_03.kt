ac2eb4b79ba136d14ca55cfc07e0125b22d556f8
Linh <linh@gami.com>
2024-11-21 14:07:15 +0700

    Update bai_03.kt
    final bai_03

--- src/bai_03.kt
+++ src/bai_03.kt
@@ -1,79 +1,113 @@
-//fun tongCacChuSo(n: Int): Int{
-//    var so = n
-//    var tong:Int = 0
-//    while (so > 0) {
-//        tong += so % 10
-//        so /= 10
-//    }
-//    return tong
-//}
-//fun thuaSoNguyenTo() {
-//    var n: Int
-//    var s:String? = readln()
-//    if (s!=null){
-//        n=s.toInt()
-//        println("Các thừa số nguyên tố của $n là:")
-//        var i : Int = 2
-//        while (i!=1) {
-//            if (n % i == 0){
-//                print("$i, ")
-//                n/=i
-//            }
-//            else
-//                i++
-//        }
-//        println()
-//    }
-//
-//}
-//
-//fun lietKeUocSo(){
-//    var n: Int
-//    var s:String? = readln()
-//    if(s!=null){
-//        n=s.toInt()
-//        print("Các ước số của $n là: ")
-//        for (i in 1..n){
-//            if (n % i == 0)
-//                print("$i là ước số")
-//        }
-//        println()
-//    }
-//fun soNguyenTo(m:Int):Boolean{
-//    if (m<2)
-//        return false
-//    for (i in 2 .. kotlin.math.sqrt(m.toDouble()).toInt() ){
-//        if ( m % i == 0){
-//            return false
-//        }
-//        return true
-//    }
-//}
-//fun lietKeSNT(n:Int):Boolean{
-//    for (i in 2 .. n/2){
-//        if (n % i == 0){
-//        return false
-//        }
-//    }
-//    return true
-//    }
-//}
-//fun main(args: Array<String>) {
-//    while (true) {
-//        try {
-//            println("Hãy nhập 1 số nguyên dương: ")
-//            var n: Int? = readLine()?.toInt()
-//            if (n!= null) {
-//                println("Vui lòng nhập số nguyên dương: ")
-//                continue
-//            }
-//            println("Tổng các chữ số của $n là ${tongCacChuSo(n)} ")
-//            println("Phân tích $n thành thừa số nguyên tố là ${thuaSoNguyenTo(n)}")
-//            println("Các ước số của $n: ${lietKeUocSo(n)}")
-//            println("")
-//            break
-//        } catch (e: Exception) {
-//            println("Vui lòng nhập 1 số hợp lệ!")
-//        }
-//    }
-//}
+fun main(args: Array<String>) {
+    while (true)
+    {
+        var choose:Int;
+        println("1. Tính tổng các chữ số của n")
+        println("2. Phân tích n thành các thừa số nguyên tố")
+        println("3. Liệt kê các ước số của n")
+        println("4. Liệt kê các ước số là nguyên tố của n")
+        println("0. Thoát")
+        var s:String? = readLine();
+        if(s!=null)
+        {
+            choose = s.toInt()
+            when(choose)
+            {
+                1 -> tinhTong()
+                2 -> phanTich()
+                3 -> lietKe()
+                4 -> lietKeSnt()
+                else -> break
+            }
+        }
+    }
+
+}
+
+fun tinhTong()
+{
+    var n:Int;
+    var sum:Int = 0;
+    var s:String? = readLine();
+    if(s!=null)
+    {
+        n = s.toInt();
+        while (n != 0)
+        {
+            sum+=n%10;
+            n/=10;
+        }
+    }
+    println("Tổng các chữ số của n là :$sum")
+}
+fun phanTich()
+{
+    var n:Int;
+    var s:String? = readLine();
+    if(s!=null)
+    {
+        n = s.toInt();
+        print("Các thừa số nguyên tố của $n là: ")
+        var i:Int = 2
+        while (n != 1)
+        {
+            if(n%i==0)
+            {
+                print("$i, ")
+                n/=i
+            }
+            else
+                i++
+        }
+        println()
+    }
+
+}
+
+fun lietKe()
+{
+    var n:Int;
+    var s:String? = readLine();
+    if(s!=null)
+    {
+        n = s.toInt();
+        print("Các ước số của $n là: ")
+        for(i in 1..n)
+        {
+            if(n%i==0)
+            {
+                print("$i,")
+            }
+        }
+        println()
+    }
+}
+
+fun lietKeSnt()
+{
+    var n:Int;
+    var s:String? = readLine();
+    if(s!=null)
+    {
+        n = s.toInt();
+        print("Các ước số là số nguyên tố của của $n là: ")
+        for(i in 1..n)
+        {
+            if(n%i==0 && checkSnt(i) == true)
+            {
+                print("$i,")
+            }
+        }
+        println()
+
+    }
+}
+fun checkSnt(n:Int): Boolean
+{
+    for(i in 2..n/2)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
